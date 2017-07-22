6834c69ec7e1a37e73f62afbbeffa59e623c717a
%= GitConfig.user_name % <atom.colour@gmail.com>
2017-07-22 13:18:34 -0700

    Get drag into window working

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ app.on('ready', () => {
   const file = 'file://' + __dirname + '/index.html'
 
   mainWindow.loadURL(file)
-  mainWindow.webContents.on('will-navigate'), (e, url) => {
+  mainWindow.webContents.on('will-navigate', (e, url) => {
     e.preventDefault();
 
     mainWindow.webContents.send('open-file', url.slice(7))
-  }
+  })
 })
