801db0340ac326befd4168fe57a333f97bac808a
%= GitConfig.user_name % <atom.colour@gmail.com>
2017-07-22 13:01:37 -0700

    Initial draft code push Electron Workshop

---
+++ index.js
@@ -0,0 +1,14 @@
+const { app, BrowserWindow } = require('electron')
+
+app.on('ready', () => {
+  const mainWindow = new BrowserWindow({})
+
+  const file = 'file://' + __dirname + '/index.html'
+
+  mainWindow.loadURL(file)
+  mainWindow.webContents.on('will-navigate'), (e, url) => {
+    e.preventDefault();
+
+    mainWindow.webContents.send('open-file', url.slice(7))
+  }
+})
